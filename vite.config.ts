f354828775802dd395a5b675b125e43dd5280a30
Armand-VINCENT <armand.vincent@etu.unilim.fr>
2026-01-15 09:53:25 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/PortolioKinetik/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
